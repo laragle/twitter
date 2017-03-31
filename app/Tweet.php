af3647fb272050b6ac4f47db43f08c27507009c6
Darwin Luague <darwinluague9001@gmail.com>
2017-03-31 15:22:19 +0800

    added tweet url accessor

--- app/Tweet.php
+++ app/Tweet.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    //
+    /**
+     *  Get the tweet url
+     * 
+     *  @return string
+     */
+    public function getUrlAttribute()
+    {
+        return route('user.tweet.show', ['user' => $this->user->slug, 'id' => $this->id]);
+    }
 }
